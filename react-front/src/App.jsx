399d65a4a25eb0266300fe4695e34b8ee03df28c
Hamza Ait Addi <aitaddihamza2023@gmail.com>
2023-10-16 21:05:29 +0200

    create react views components and routes

---
+++ react-front/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+    return (
+        <div className="text-xl text-blue-600 text-center">Hell world ! </div>
+    );
+};
+
+export default App;
---
+++ react-front/src/router.jsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import GuestLayout from "./layouts/GuestLayout";
+import Users from "./views/user/Users";
+import Dashboard from "./views/user/Dashboard";
+import UserForm from "./views/user/UserForm";
+import Login from "./views/auth/Login";
+import Register from "./views/auth/Register";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/users",
+                element: <Users />,
+            },
+            {
+                path: "/dashboard",
+                element: <Dashboard />,
+            },
+            {
+                path: "/new",
+                element: <UserForm key="createUser" />,
+            },
+            {
+                path: "/edit/:id",
+                element: <UserForm key="editUser" />,
+            },
+        ],
+    },
+    {
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+        ],
+    },
+]);
+
+export default router;
