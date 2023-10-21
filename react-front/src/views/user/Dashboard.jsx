25dcab7d733d6a07a54c75ec321ddb78a20502f8
Hamza <aitaddi.hamza365@gmail.com>
2023-10-21 20:03:36 +0200

    fixing some bugs and add some stuff to dashboard

--- react-front/src/views/user/Dashboard.jsx
+++ react-front/src/views/user/Dashboard.jsx
@@ -1,28 +1,35 @@
 import { Link } from "react-router-dom";
 import { useValue } from "../../Context";
 import avatarPng from "../../images/ava.png";
+import { useEffect } from "react";
 
 const Dashboard = () => {
-    const { users } = useValue();
-    const usersCount = users.length;
+    const { users, getUsers, loading } = useValue();
+
+    useEffect(() => {
+        getUsers();
+    }, []);
     return (
         <div className="p-8 flex justify-between gap-2 items-center">
-            <div className="flex items-center p-2 rounded bg-white-500 text-black  text-xl text-center  font-medium w-[200px] shadow-sm border border-solid ">
-                <figure className="w-[60px]">
-                    <img src={avatarPng} className="" />
-                </figure>
-                <h1 className="w-1/2">
-                    {usersCount} users
-                    <p>
-                        <Link
-                            className="text-sm text-blue-500 hover:underline"
-                            to="/users"
-                        >
-                            see more
-                        </Link>
-                    </p>
-                </h1>
-            </div>
+            {loading && <p>Loading...</p>}
+            {!loading && users && (
+                <div className="flex items-center p-2 rounded bg-white-500 text-black  text-xl text-center  font-medium w-[200px] shadow-sm border border-solid ">
+                    <figure className="w-[60px]">
+                        <img src={avatarPng} className="" />
+                    </figure>
+                    <h1 className="w-1/2">
+                        {users.length} users
+                        <p>
+                            <Link
+                                className="text-sm text-blue-500 hover:underline"
+                                to="/users"
+                            >
+                                see more
+                            </Link>
+                        </p>
+                    </h1>
+                </div>
+            )}
         </div>
     );
 };
